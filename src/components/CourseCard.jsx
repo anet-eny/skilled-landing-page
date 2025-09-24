11a5fa37c2d504ffdf8551def17a87006c9a4f9a
anet-eny <anet-eny@gmail.com>
2025-09-24 16:32:10 +0200

    Style CourseCard component

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -1,3 +1,14 @@
-export default function CourseCard() {
-  return <></>;
+import Button from "./Button";
+
+export default function CourseCard({ icon, title, description }) {
+  return (
+    <div className="px-8 pt-18 pb-10">
+      <div className="w-14 rounded-full bg-gradient-to-b from-orange-500 to-fuchsia-500">
+        {icon}
+      </div>
+      <h3 className="text-preset-4">{title}</h3>
+      <p className="text-preset-5 my-6">{description}</p>
+      <Button />
+    </div>
+  );
 }
