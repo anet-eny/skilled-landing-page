acc0985a2367b648ad0203d3ecb4f199e2da8cfb
anet-eny <anet-eny@gmail.com>
2025-09-25 20:14:36 +0200

    Update styles of CourseCard and HighlightCard components

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,12 @@
 export default function Button({ children, variant = "link" }) {
-  const base = "text-preset-7 py-4 px-8 rounded-full";
+  const base = "text-preset-7 rounded-full";
   const variants = {
     link: "text-rose-400 hover:text-rose-300",
-    black: "text-white bg-blue-950 hover:opacity-60",
+    black: "py-4 px-8 text-white bg-blue-950 hover:opacity-60",
     orange:
-      "text-white bg-gradient-to-b from-gradient1-start to-gradient1-end hover:opacity-60",
+      "py-4 px-8 text-white bg-gradient-to-b from-gradient1-start to-gradient1-end hover:opacity-60",
     purple:
-      "text-white bg-gradient-to-b from-gradient2-start to-gradient2-end hover:opacity-60",
+      "py-4 px-8 text-white bg-gradient-to-b from-gradient2-start to-gradient2-end hover:opacity-60",
   };
 
   return <button className={`${base} ${variants[variant]}`}>{children}</button>;
--- src/components/HighlightCard.jsx
+++ src/components/HighlightCard.jsx
@@ -1,6 +1,6 @@
 export default function HighlightCard() {
   return (
-    <div className="w-full p-8 bg-gradient-to-b from-gradient1-start to-gradient1-end">
+    <div className="w-full p-8 mb-6 rounded-2xl bg-gradient-to-b from-gradient1-start to-gradient1-end">
       <h2 className="text-white text-preset-4 text-2xl lg:text-preset-3">
         Check out our most popular courses!
       </h2>
