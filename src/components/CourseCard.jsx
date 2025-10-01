4168c7d0b35bccaa29da325451d9c5c65c09fc44
anet-eny <anet-eny@gmail.com>
2025-10-01 21:20:01 +0200

    Add background gradient to body

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -2,7 +2,7 @@ import Button from "./Button";
 
 export default function CourseCard({ icon, title, description }) {
   return (
-    <div className="relative px-8 pt-16 pb-10 mb-6 rounded-2xl">
+    <div className="relative px-8 pt-16 pb-10 mb-6 rounded-2xl bg-white">
       <div className="absolute -top-7 w-14 h-14 rounded-full bg-gradient-to-b from-gradient1-start to-gradient1-end flex items-center justify-center">
         {icon}
       </div>
