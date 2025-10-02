56d8813ea782894349e08fe7165e1843d956d41f
anet-eny <anet-eny@gmail.com>
2025-10-02 20:38:46 +0200

    Style Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,13 @@
+import Button from "./Button";
+import logoLight from "../assets/logo-light.svg";
+
 export default function Footer() {
-  return <></>;
+  return (
+    <footer className="bg-blue-950">
+      <div className="flex items-center justify-between max-w-6xl px-5 py-6 md:px-11 md:py-6 lg:px-20 mx-auto">
+        <img src={logoLight} className="max-w-20 md:max-w-none" alt="" />
+        <Button variant="purple">Get started</Button>
+      </div>
+    </footer>
+  );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,17 @@ import Button from "../components/Button";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import CoursesSection from "../components/CoursesSection";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
-    <main className="max-w-6xl mx-auto">
-      <Header />
-      <Hero />
-      <CoursesSection />
-    </main>
+    <>
+      <main className="max-w-6xl mx-auto">
+        <Header />
+        <Hero />
+        <CoursesSection />
+      </main>
+      <Footer />
+    </>
   );
 }
